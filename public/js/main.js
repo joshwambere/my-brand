d0ce5e7d71581e273b291e59dc71d9c3e6c552c4
josh <joshwambere@gmail.com>
2020-07-19 14:51:32 +0200

    fixed landing page profile image section size&position and personal info section spacing

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
+
 const humburgerIcn=document.getElementById('humburger');
 const dropMenu=document.getElementById('JdMenu');
 
 function toggleHumburger(){
     humburgerIcn.classList.toggle('active');
-    dropMenu.classList.toggle('jdNavMenuHumburger');
+    dropMenu.classList.toggle('active');
 }
 humburgerIcn.addEventListener('click',toggleHumburger);
 
