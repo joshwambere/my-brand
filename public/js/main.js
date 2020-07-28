cc747be745708be44ccc99fe46e3727ab09ae14e
josh <joshwambere@gmail.com>
2020-07-28 11:09:32 +0200

    form validaton added

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,70 @@
+const humburgerIcn = document.getElementById('humburger');
+const dropMenu = document.getElementById('JdMenu');
 
-const humburgerIcn=document.getElementById('humburger');
-const dropMenu=document.getElementById('JdMenu');
-
-function toggleHumburger(){
+function toggleHumburger() {
     humburgerIcn.classList.toggle('active');
     dropMenu.classList.toggle('active');
 }
-humburgerIcn.addEventListener('click',toggleHumburger);
+humburgerIcn.addEventListener('click', toggleHumburger);
 
-//on click humbergure click
-//validate the form
-const loginBtn=document.getElementById('login');
-console.log(loginBtn);
-var form=document.forms.myForm;
-form.onsubmit=function (){
-    var username =document.getElementById('username').value;
-    var pwd=document.getElementById('pwd').value;
-    if(username=='johnson' && pwd=='okayfine'){
-        console.log('yes');
-        window.location.href ='../../app/html/admin.html';
+
+//validate the login form
+const name = document.getElementById('username');
+const pwd = document.getElementById('pwd');
+const form = document.getElementById('myForm')
+const userError = document.getElementById('UsernameError')
+const pwdError = document.getElementById('PwdError')
+form.addEventListener('submit', (e) => {
+    var messageUser = [];
+    var messagepwd = [];
+    if (name.value === '' || name == null) {
+        messageUser.push('username is required');
     }
-}
+
+    if (pwd.value === '' || pwd.value == null) {
+        messagepwd.push('password is required');
+    } else {
+        if (pwd.value.length < 6) {
+            messagepwd.push('password must be 6 char long');
+        }
+    }
+
+    if (messageUser.length > 0 || messagepwd.length > 0) {
+        e.preventDefault();
+        userError.innerHTML = messageUser;
+        pwdError.innerHTML = messagepwd;
+
+    }
+})
+
+//validate signup
+
+const RegisterPwd = document.getElementById('pwd');
+const confirmPwd = document.getElementById('rePwd')
+const registerpwdError = document.getElementById('PwdError')
+const confirmPwdError = document.getElementById('rePwdError')
+const registerForm = document.getElementById('myForm');
+
+registerForm.addEventListener('submit', (e) => {
+    var messageConfirmPwd = [];
+    var messagePwd = [];
+    if (RegisterPwd.value === '' || RegisterPwd.value == null) {
+        messagePwd.push('password is required');
+    } else {
+        if (RegisterPwd.value.length < 6) {
+            messagePwd.push('password must be 6 char long');
+        } else {
+            if (confirmPwd.value != RegisterPwd.value) {
+                messageConfirmPwd.push("Password doesn't match");
+            }
+        }
+    }
+
+
+    if (messageConfirmPwd.length > 0 || messagePwd.length > 0) {
+        e.preventDefault();
+        confirmPwdError.innerHTML = messageConfirmPwd;
+        registerpwdError.innerHTML = messagePwd;
+
+    }
+})
