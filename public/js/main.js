aeb2017a308f11bdc05eadee159d665f156e38e6
josh <joshwambere@gmail.com>
2020-07-29 09:40:37 +0200

    fixed login redirection issue

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,7 @@ const form = document.getElementById('myForm')
 const userError = document.getElementById('UsernameError')
 const pwdError = document.getElementById('PwdError')
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     var messageUser = [];
     var messagepwd = [];
     if (name.value === '' || name == null) {
@@ -34,37 +35,8 @@ form.addEventListener('submit', (e) => {
         userError.innerHTML = messageUser;
         pwdError.innerHTML = messagepwd;
 
-    }
-})
-
-//validate signup
-
-const RegisterPwd = document.getElementById('pwd');
-const confirmPwd = document.getElementById('rePwd')
-const registerpwdError = document.getElementById('PwdError')
-const confirmPwdError = document.getElementById('rePwdError')
-const registerForm = document.getElementById('myForm');
-
-registerForm.addEventListener('submit', (e) => {
-    var messageConfirmPwd = [];
-    var messagePwd = [];
-    if (RegisterPwd.value === '' || RegisterPwd.value == null) {
-        messagePwd.push('password is required');
-    } else {
-        if (RegisterPwd.value.length < 6) {
-            messagePwd.push('password must be 6 char long');
-        } else {
-            if (confirmPwd.value != RegisterPwd.value) {
-                messageConfirmPwd.push("Password doesn't match");
-            }
-        }
-    }
-
-
-    if (messageConfirmPwd.length > 0 || messagePwd.length > 0) {
-        e.preventDefault();
-        confirmPwdError.innerHTML = messageConfirmPwd;
-        registerpwdError.innerHTML = messagePwd;
-
+    }else{
+        window.location.href='../../app/html/admin.html', true;
+        
     }
 })
