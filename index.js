af3d22db7dee8aaa81f285c4a47a9982f1f01ce9
josh <joshwambere@gmail.com>
2020-08-24 12:54:32 +0200

    success & error messages added on request on post endpoint

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const router = require('./routes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("./routes");
+require("dotenv").config();
 
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
-mongoose
-  .connect(
-    process.env.DB_CONNECT,
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    const app = express();
-    app.use(bodyParser.json());
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }).then(() => {
+  const app = express();
+  app.use(bodyParser.json());
 
-    app.get('/', (req, res) => {
-      return res.send('WELCOME TO MY BRAND');
-    });
-    
-    app.use(router);
-    app.listen(PORT, () => {
-      console.log('connection started!');
-    });
+  app.get("/", (req, res) => {
+    return res.send("WELCOME TO MY BRAND");
   });
+  app.use(router);
+  app.listen(PORT, () => {
+    console.log("connection started!");
+  });
+});
